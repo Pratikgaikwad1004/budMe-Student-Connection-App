c71fbacc8840021c438db84627344d01d97c01ee
Pratik Gaikwad <pratikgaikwad1004@gmail.com>
2023-04-21 00:15:59 +0530

    Chat added database

--- src/views/MyChats.vue
+++ src/views/MyChats.vue
@@ -38,10 +38,11 @@ export default {
         this.socket.emit("login", user);
         this.socket.emit("join", this.sendTo);
         this.socket.on('receive', (data) => {
-                console.log("mess", data);
-                // this.receviedMsg = data.message;
-                this.messages.push({type: "incomming", msg: data.message});
-            })
+            console.log("mess", data);
+            // this.receviedMsg = data.message;
+            this.messages.push({ type: "incomming", msg: data.message, from: data.from, to: data.to });
+            this.addMsgInDB(data.from, data.to, data.message, "incomming")
+        })
         // console.log(this.sendTo);
         // console.log(this.messages);
     },
@@ -49,17 +50,47 @@ export default {
     methods: {
         onReceiveMsg() {
             // console.log("rinning");
-            
+
         },
         onChangeMessage(e) {
             this.message = e.target.value;
             // console.log(this.message);
         },
+        addMsgInDB(from, to, msg, type) {
+            try {
+                const myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/json");
+
+                const raw = JSON.stringify({
+                    "from": from,
+                    "to": to,
+                    "msg": msg,
+                    "type": type
+                });
+
+                const requestOptions = {
+                    method: 'POST',
+                    headers: myHeaders,
+                    body: raw,
+                    redirect: 'follow'
+                };
+
+                fetch("http://localhost:3000/api/v1/msg/newmsg", requestOptions)
+                    .then(response => response.json())
+                    .then(result => console.log(result))
+                    .catch(error => console.log('error', error));
+            } catch (error) {
+                console.log(error);
+            }
+        },
         onSend(e) {
             const user = localStorage.getItem("user")
             // console.log("aaa");
             this.socket.emit("send", user, this.sendTo, this.message)
-            this.messages.push({type: "outgoing", msg: this.message, from: user, to: this.sendTo})
+            console.log("from", user, "to", this.sendTo);
+            const time = Date.now();
+            this.messages.push({ type: "outgoing", msg: this.message, from: user, to: this.sendTo, time: time });
+            this.addMsgInDB(user, this.sendTo, this.message, "outgoing")
             // this.onReceiveMsg();
             this.message = "";
             e.target.value = "";
@@ -100,4 +131,5 @@ export default {
     .chat-dashboard {
         flex-basis: 100%;
     }
-}</style>
+}
+</style>
