6eb33a62b5f499401ef707d73ad0f4fd245ac16d
Upamanyu <udwable@gmail.com>
2023-04-22 00:23:35 +0530

    Completed Home view frontend

---
+++ src/components/HomeEventCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <div class="card mb-3" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Project Title</h5>
+                <p class="card-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore facere architecto consequatur quam exercitationem hic enim quisquam maiores</p>
+                <v-btn color="#8e86d9" text>
+                        Remove
+                </v-btn>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped></style>
