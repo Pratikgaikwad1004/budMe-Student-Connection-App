08014714ee0b9576f69c137146532f6584179dbf
Upamanyu <udwable@gmail.com>
2023-04-22 00:43:29 +0530

    Succesfully added event Registration Form

--- src/views/eventRegistrationForm.vue
+++ src/views/eventRegistrationForm.vue
@@ -1,31 +1,39 @@
 <template>
-    <div class="container">
-        <div class="form-group">
-            <label for="eventTitle">Title</label>
-            <input type="text" class="form-control" id="eventTitle" v-model="title" placeholder="Event Title">
-        </div>
-        <div class="form-group">
-            <label for="institute">Institute</label>
-            <input type="text" class="form-control" id="institute" v-model="institute" placeholder="Enter Institue Name">
-        </div>
-        <div class="form-group">
-            <label for="location">Location</label>
-            <input type="email" class="form-control" id="location" v-model="Location" placeholder="ex. Pune , Mumbai">
-        </div>
-        <div class="form-group">
-            <label for="time">Date & Time</label>
-            <input type="datetime-local" class="form-control" id="time" v-model="time" placeholder="enter time seperated with comma and space">
-        </div>
+    <div class="body" style="height : 100vh ; width: 100vw; border:2px solid black ">
+        <div class="main">
+            <div class="container">
+                <h2>Event Registration Form</h2>
+                <div class="form-group my-1">
+                    <label for="eventTitle">Title</label>
+                    <input type="text" class="form-control" id="eventTitle" v-model="title" placeholder="Event Title">
+                </div>
+                <div class="form-group my-1">
+                    <label for="institute">Institute</label>
+                    <input type="text" class="form-control" id="institute" v-model="institute"
+                        placeholder="Enter Institue Name">
+                </div>
+                <div class="form-group my-1">
+                    <label for="location">Location</label>
+                    <input type="email" class="form-control" id="location" v-model="Location"
+                        placeholder="ex. Pune , Mumbai">
+                </div>
+                <div class="form-group my-1">
+                    <label for="time">Date & Time</label>
+                    <input type="datetime-local" class="form-control" id="time" v-model="time"
+                        placeholder="enter time seperated with comma and space">
+                </div>
 
-        <label for="eventImage">Event Image</label>
-        <input type="file" class="form-control-file" @change="image" id="eventImage"> <br>
+                <label for="eventImage">Event Image</label>
+                <input type="file" class="form-control-file my-1" @change="image" id="eventImage"> <br>
 
-        <div class="form-group">
-            <label for="eventDescription">Event Description</label>
-            <input class="form-control" id="eventDescription" v-model="description" placeholder="Enter Event Details">
+                <div class="form-group my-1">
+                    <label for="eventDescription">Event Description</label>
+                    <textarea class="form-control" name="eventDescription" id="eventDescription" v-model="description" cols="30" rows="10"></textarea>
+                </div>
+
+                <button type="submit" class="btn" @click="onSubmit">Submit</button>
+            </div>
         </div>
-
-        <button type="submit" class="btn" @click="onSubmit">Submit</button>
     </div>
 </template>
 
@@ -64,8 +72,20 @@ export default {
 
                 fetch("http://localhost:3000/api/v1/event/addevent", requestOptions)
                     .then(response => response.json())
-                    .then(result => console.log(result))
-                    .catch(error => console.log('error', error));
+                    .then(result => {
+                        if(result.success){
+                            this.img = "";
+                            this.title = "";
+                            this.description ="";
+                            this.institute="";
+                            this.time=""; 
+                            this.Location="";
+                        }
+                        else{
+                            alert(result.error);
+                        }
+                    })
+                    .catch(error => console.log('error', error));            
             } catch (error) {
                 console.log(error);
             }
@@ -77,17 +97,6 @@ export default {
             try {
                 var myHeaders = new Headers();
                 myHeaders.append("Content-Type", "multipart/form-data");
-
-                // var raw = JSON.stringify({
-                //     "img": "https://unsplash.com/photos/YvpLGlwNISY",
-                //     "title": "Nature",
-                //     "description": "aaaaaaaaaaaaaaaaaaaaaaa",
-                //     "institute": "SIT",
-                //     "time": [
-                //         "10:00am",
-                //         "11:00pm"
-                //     ]
-                // });
 
                 var requestOptions = {
                     method: 'POST',
@@ -101,11 +110,10 @@ export default {
                         this.img = result.filepath;
                     })
                     .catch(error => console.log('error', error));
-                
+
             } catch (error) {
-             console.log(error);   
+                console.log(error);
             }
-            // console.log(this.img)
         }
     },
 }
@@ -114,5 +122,42 @@ export default {
 <style lang="scss" scoped>
 .btn {
     background-color: #b4aee8;
+}
+
+.body{
+    height : 100vh ; 
+    width: 100vw;
+     border:2px solid black;
+     display: flex;;
+     justify-content: center;
+     align-items: center;
+     
+}
+.main{
+    width: 40vw;
+    height: 90vh;
+    // border: 2px solid red;
+    padding: 20px;
+    overflow: auto;
+    box-shadow: 0px 0px 7px 0px gray;
+    border-radius: 10px;
+}
+
+.container h2{
+    text-align: center;
+}
+
+.eventDescription{
+    height:10vh
+}
+
+@media only screen and (max-width: 800px) {
+    .container h2{
+        font-size: 25px
+    }
+    
+    .main{
+        width: 70vw;
+    }
 }
 </style>
