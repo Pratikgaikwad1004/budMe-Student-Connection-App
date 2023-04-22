19e9fb596fd6923a5113f6c2ddf8e7a9a02c32f2
divyamdinker007 <71220418+divyamdinker007@users.noreply.github.com>
2023-04-22 18:01:36 +0530

    home page

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -1,29 +1,56 @@
 <template>
-    <div>
-        <div class="card" style="width: 80%;" >
-            <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
-                    content.</p>
-                    <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-bookmark</v-icon>
-              </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-share-variant</v-icon>
-              </v-btn>
-            </div>
-        </div>
+  <div>
+    <div class="card" style="width: 80%;">
+      <img class="card-img-top" src="../assets/logo.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">{{ cardData.title }}</h5>
+        <p class="card-text">{{ cardData.text }}</p>
+        <v-btn icon @click="handleLike">
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <v-btn icon @click="handleShare">
+          <v-icon>mdi-share-variant</v-icon>
+        </v-btn>
+        <v-btn icon @click="handleSave">
+          <v-icon>mdi-bookmark</v-icon>
+        </v-btn>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-export default {
+import axios from 'axios';
 
-}
+export default {
+  data() {
+    return {
+      cardData: {},
+    };
+  },
+  created() {
+    axios.get('/api/card').then((response) => {
+      this.cardData = response.data;
+    });
+  },
+  methods: {
+    handleLike() {
+      axios.post('/api/card/like', { id: this.cardData.id }).then((response) => {
+        console.log(response.data);
+      });
+    },
+    handleShare() {
+      axios.post('/api/card/share', { id: this.cardData.id }).then((response) => {
+        console.log(response.data);
+      });
+    },
+    handleSave() {
+      axios.post('/api/card/save', { id: this.cardData.id }).then((response) => {
+        console.log(response.data);
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
